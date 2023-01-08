30860336e8bee1a0c02a6f3737749fa41a9e22ba
Rita Ferreira <rialf.ferreira@gmail.com>
2023-01-08 11:21:02 -0300

    Application completed.

---
+++ src/main/java/com/diazero/developerchallenge/model/Incident.java
@@ -0,0 +1,33 @@
+package com.diazero.developerchallenge.model;
+
+import lombok.*;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Data
+@EqualsAndHashCode
+@Entity
+@Table(name="incidents")
+public class Incident implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long idIncident;
+    @NotBlank
+    @Column(name = "name")
+    private String name;
+    @NotBlank
+    @Column(name = "description")
+    private String description;
+    @Column(name = "createdAt")
+    private LocalDateTime createdAt = LocalDateTime.now();
+    @Column(name = "updateAt")
+    private LocalDateTime updateAt;
+    @Column(name = "closeAt")
+    private LocalDateTime closeAt;
+}
---
+++ src/main/java/com/diazero/developerchallenge/model/exception/GeneralException.java
@@ -0,0 +1,11 @@
+package com.diazero.developerchallenge.model.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR)
+public class GeneralException extends RuntimeException {
+    public GeneralException(String message){
+        super(message);
+    }
+}
